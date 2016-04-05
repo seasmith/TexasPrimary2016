cee7f2d8d0d80771d6727e975839c8d2bdf78be7
Luke Smith <lukedansmi@sbcglobal.net>
2016-04-04 23:21:49 -0500

    Condesning functions into cat.functions.R

--- FRED.example2.R
+++ FRED.example2.R
@@ -1,24 +1,35 @@
 # fred.data.example2
 library(ggplot2, quietly = T)
 library(dplyr, quietly = T)
+library(plyr, quietly = T)
 library(rvest, quietly = T)
 library(choroplethr, quietly = T)
 library(choroplethrMaps, quietly = T)
 library(gridExtra, quietly = T)
+library(lubridate, quietly = T)
+source("cat.functions.R")
 data("county.regions")
+
+
+# Load Geo Data -----------------------------------------------------------
+
+tx.regions                <- filter(county.regions, state.name == "texas") %>%
+                             select(region, "CountyName" = county.name)
 
 
 # Estimated Household Income ----------------------------------------------
 
 
-emhi.series               <- fred.series2[fred.series2$Category == "Estimate of Median Household Income", ]
-emhi.series$County        <- tolower(emhi.series$County)
-tx.regions                <- filter(county.regions, state.name == "texas") %>%
-                             select(region, "CountyName" = county.name)
-colnames(emhi.series)[11] <- "CountyName"
+# emhi.series               <- fred.series2[fred.series2$Category == "Estimate of Median Household Income", ]
+# emhi.series$County        <- tolower(emhi.series$County)
+# tx.regions                <- filter(county.regions, state.name == "texas") %>%
+#                              select(region, "CountyName" = county.name)
+# colnames(emhi.series)[11] <- "CountyName"
+# 
+# # Find all SeriesID matching the above filtered data's SeriesID
+# emhi.obs  <- fred.obs2[names(fred.obs2) == emhi.series$SeriesID]
 
-# Find all SeriesID matching the above filtered data's SeriesID
-emhi.obs  <- fred.obs2[names(fred.obs2) == emhi.series$SeriesID]
+emhi.obs <- obs.catcher(fred.series2, fred.obs2, "Estimate of Median Household Income")
 
 # Find 2014 values (last row)
 loop.list <- list()
@@ -48,36 +59,28 @@ choro_emhi
 # Per Capita Personal Income ----------------------------------------------
 
 
-pcpi.series               <- fred.series1[fred.series1$Category == "Per Capita Personal Income", ]
-pcpi.series$County        <- tolower(pcpi.series$County)
-tx.regions                <- filter(county.regions, state.name == "texas") %>%
-    select(region, "CountyName" = county.name)
-colnames(pcpi.series)[11] <- "CountyName"
+# Get observations for "Per Capita Personal Income" into a table with county names
+pcpi.obs          <- obs.catcher(fred.series1, fred.obs1, "Per Capita Personal Income")
+pcpi.table        <- cat.tabler(pcpi.obs)
+pcpi.table.subset <- pcpi.table[year(pcpi.table$Date) == "2013", -1]
+pcpi.search       <- names(pcpi.table.subset) %>%
+                     paste(collapse = "|")
+pcpi.counties     <- fred.series1[grep(pcpi.search, fred.series1$SeriesID), 'County'] %>%
+                     tolower() ## Rename 'County' to 'CountyName' !!!!!!!!!!!!!
+pcpi.data         <- cbind(PCPI = as.character(unlist(pcpi.table.subset)), CountyName = pcpi.counties) %>%
+                     as.data.frame()
+#!!!!!!!Start here!!!!!!!!~
+# Insert 'region'
+pcpi.data      <- left_join(pcpi.data, tx.regions, "CountyName")
+pcpi.data$PCPI <- as.numeric(as.character(pcpi.data$PCPI))
 
-# Find all SeriesID matching the above filtered data's SeriesID
-pcpi.obs  <- fred.obs1[names(fred.obs1) == pcpi.series$SeriesID]
+# Rank the values
+pcpi.data <- within(pcpi.data, value <- as.factor(cut(PCPI, quantile(PCPI), include.lowest = T, labels = F)))
 
-# Find 2014 values (last row)
-loop.list <- list()
-pcpi.obs  <- lapply(seq_along(pcpi.obs), function(x){
-    
-    to.get    <- dim(pcpi.obs[[x]])[1]
-    loop.df   <- data.frame(SeriesID = names(pcpi.obs[x]),
-                            PCPI     = pcpi.obs[[x]][to.get, 2])
-    loop.list[[x]] <- loop.df
-}) %>% ldply
-
-# now create the data frame with 'region' identifier
-pcpi.obs$CountyName <- pcpi.series[pcpi.obs$SeriesID == pcpi.obs$SeriesID, 'CountyName']
-pcpi.obs            <- left_join(pcpi.obs, tx.regions, "CountyName")
-pcpi.obs$PCPI       <- as.numeric(as.character(pcpi.obs$PCPI))
-
-
-pcpi.obs <- within(pcpi.obs, value <- as.factor(cut(PCPI, quantile(PCPI), include.lowest = T, labels = F)))
-
+# Plot it out
 choro_pcpi     <- county_choropleth(pcpi.obs, state_zoom = "texas", legend = "Rank: Lowest to Highest", num_colors = 4) +
-    ggtitle("Per Capita Personal Income\n 2013") +
-    coord_map()
+                  ggtitle("Per Capita Personal Income\n 2013") +
+                  coord_map()
 choro_pcpi
 
 
--- cat.functions.R
+++ cat.functions.R
@@ -1,25 +1,84 @@
-cat.catcher <- function(series.table, cat){
-    cat.series               <- series.table[series.table$Category == cat, ]
-    cat.series$County        <- tolower(cat.series$County)
-    tx.regions                <- filter(county.regions, state.name == "texas") %>%
-        select(region, "CountyName" = county.name)
+# obs.catcher(series.table, obs.list, cat) --------------------------------
+
+
+# This function is required to achieve the results in 'FRED.example2.R'
+
+obs.catcher <- function(series.table, obs.list, cat){
+    cat.series               <- series.table[series.table[, 'Category'] == cat, ]
+    cat.series[, 'County']   <- tolower(cat.series[, 'County'])
     colnames(cat.series)[11] <- "CountyName"
     
     # Find all SeriesID matching the above filtered data's SeriesID
-    cat.obs  <- fred.obs1[names(fred.obs1) == cat.series$SeriesID]
+    cat.obs <- list()
+    cat.obs <- obs.list[cat.series[ , 'SeriesID']]
 }
 
+
+# cat.info(series.table) --------------------------------------------------
+
+
+# This is an optional function which allows you to explore the constraints on a 'Series's' data.
+# Specifically it examines the 'Frequency', 'Start', 'End', and 'Units' of each 'Category'.
 cat.info <- function(series.table){
 
     cat      <- unique(series.table$Category)
     infos    <- list()
     big.list <- lapply(seq_along(cat), function(x){
-            freq  <- unique(series.table[series.table$Category == cat[[x]], 'Frequency'])
-            start <- unique(series.table[series.table$Category == cat[[x]], 'Start'])
-            end   <- unique(series.table[series.table$Category == cat[[x]], 'End'])
-            units <- unique(series.table[series.table$Category == cat[[x]], 'Units'])
-            infos[[x]] <- data.frame(freq, start, end, units)
+            release <- unique(series.table[series.table$Category == cat[[x]], 'Release'])
+            freq    <- unique(series.table[series.table$Category == cat[[x]], 'Frequency'])
+            start   <- unique(series.table[series.table$Category == cat[[x]], 'Start'])
+            end     <- unique(series.table[series.table$Category == cat[[x]], 'End'])
+            units   <- unique(series.table[series.table$Category == cat[[x]], 'Units'])
+            infos[[x]] <- data.frame(release, freq, start, end, units)
     })
     names(big.list) <- cat
-    big.list
+    df              <- big.list %>% ldply
+    names(df)[1]    <- "category"
+    df
 }
+
+
+# cat.tabler(obs.list) ----------------------------------------------------
+
+
+cat.tabler <- function(obs.list){
+    x <- 1
+    while(x <= length(obs.list)){
+        if(x == 1){
+            main.frame           <- data.frame()
+            main.frame           <- obs.list[[x]]
+            names(main.frame)[2] <- names(obs.list)[1]
+            main.frame
+        }   else {
+            main.frame               <- merge(main.frame, obs.list[[x]], by = "Date")
+            names(main.frame)[x + 1] <- names(obs.list)[x]
+            main.frame
+        }
+        x = x + 1
+        main.frame
+    }
+    main.frame
+}
+
+# # tranpose, create row names column, change column headers, and assign data types 
+# main.frame2           <- t(main.frame)
+# colnames(main.frame2) <- main.frame2[1, ]
+# main.frame2           <- main.frame2[-1, ]
+
+
+# Other -------------------------------------------------------------------
+
+
+# # Find 2014 values (last row)
+# loop.list <- list()
+# pcpi.obs  <- lapply(seq_along(pcpi.obs), function(x){
+#     
+#     to.get    <- dim(pcpi.obs[[x]])[1]
+#     loop.df   <- data.frame(SeriesID = names(pcpi.obs[x]),
+#                             PCPI     = pcpi.obs[[x]][to.get, 2])
+#     loop.list[[x]] <- loop.df
+# }) %>% ldply
+
+# # good effort here
+# # it does work
+# merged <- Reduce(function(...) merge(..., all = T, by = "Date"), pcpi.obs)
