bc4f3e797968a03e9fa3bd227bfc0a0bdcfb7b0d
Luke Smith <lukedansmi@sbcglobal.net>
2016-04-06 01:48:46 -0500

    Create cat.county.scraper(), and add to scraper.functions.R, series.scraper(), and FRED.example1.R

--- FRED.example1.R
+++ FRED.example1.R
@@ -14,141 +14,144 @@ source("scraper.functions.R")
 
 # Run the Meta Data Function-----------------------------------------------
  
-        ## set 1st set of variables
+    ## set 1st set of variables
         myapi <- "" # set your API here
         fred.input1 <- c("116", "119", "330", "175")
         filter1     <- c("tx", "county")
         f.data1     <- list()
         
 ## run the 1st set
-fred.series1 <- lapply(seq_along(fred.input1), function(x){
-    
-    tryCatch({
-        f.data1[[x]] <- series.scraper(key    = myapi,
-                                    id     = fred.input1[x],
-                                    filter = filter1
-                                    )
-    }, error = function(e){
-        Sys.sleep(3.5)
-        f.data1[[x]] <- series.scraper(key    = myapi,
-                                    id     = fred.input1[x],
-                                    filter = filter1
-        )
-    })
-}) %>% ldply()
+    fred.series1 <- lapply(seq_along(fred.input1), function(x){
+        
+        tryCatch({
+            f.data1[[x]] <- series.scraper(key    = myapi,
+                                           id     = fred.input1[x],
+                                           filter = filter1)
+        }, error = function(e){
+            Sys.sleep(3.5)
+            f.data1[[x]] <- series.scraper(key    = myapi,
+                                           id     = fred.input1[x],
+                                           filter = filter1)
+        })
+    }) %>% ldply()
+
+# save it
+    save(fred.series1, file = "fred.series1.RData")
 
     ## set 2nd set of variables
-    fred.input2 <- c("346")
-    filter2     <- c("tx", "county", "income")
-    f.data2     <- list()
+        fred.input2 <- c("346")
+        filter2     <- c("tx", "county", "income")
+        f.data2     <- list()
 
 ## run the 2nd set
-fred.series2 <- lapply(seq_along(fred.input2), function(x){
-    
-    tryCatch({
-        f.data2[[x]] <- series.scraper(key    = myapi,
-                                    id     = fred.input2[x],
-                                    filter = filter2
-                                    )
-    }, error = function(e){
-        Sys.sleep(3.5)
-        f.data2[[x]] <- series.scraper(key    = myapi,
-                                    id     = fred.input2[x],
-                                    filter = filter2
-        )
-    })
-}) %>% ldply()
+    fred.series2 <- lapply(seq_along(fred.input2), function(x){
+        
+        tryCatch({
+            f.data2[[x]] <- series.scraper(key    = myapi,
+                                        id     = fred.input2[x],
+                                        filter = filter2
+                                        )
+        }, error = function(e){
+            Sys.sleep(3.5)
+            f.data2[[x]] <- series.scraper(key    = myapi,
+                                        id     = fred.input2[x],
+                                        filter = filter2
+            )
+        })
+    }) %>% ldply()
 
+# save it
+    save(fred.series2, file = "fred.series2.RData")
 
 # Clean up the Meta Data --------------------------------------------------
 
-    ## get the right county names
-    right.counties <- c("Deaf Smith", "El Paso", "Fort Bend",
-                        "Jeff Davis", "Jim Hogg", "Jim Wells",
-                        "La Salle","Live Oak", "Palo Pinto",
-                        "Red River", "San Augustine", "San Jacinto",
-                        "San Patricio", "San Saba", "Tom Green",
-                        "Val Verde", "Van Zandt")
-
-## 1st series unique 'Category' values
-uis1           <- unique(fred.series1$Category)
-    
-# clean up 'CountyName' and 'Category'
-fred.series1[fred.series1$Category == uis1[[3]], 'CountyName'] <- right.counties
-fred.series1[fred.series1$Category == uis1[[4]], 'CountyName'] <- right.counties
-fred.series1[fred.series1$Category == uis1[[6]], 'CountyName'] <- right.counties
-fred.series1[fred.series1$Category == uis1[[8]], 'CountyName'] <- right.counties
-fred.series1[fred.series1$Category == uis1[[10]], 'CountyName'] <- right.counties
-fred.series1[fred.series1$Category == uis1[[12]], 'CountyName'] <- right.counties
-fred.series1[fred.series1$Category == uis1[[14]], 'CountyName'] <- right.counties
-
-fred.series1[fred.series1$Category == uis1[[3]], 'Category'] <- uis1[[1]]
-fred.series1[fred.series1$Category == uis1[[4]], 'Category'] <- uis1[[2]]
-fred.series1[fred.series1$Category == uis1[[6]], 'Category'] <- uis1[[5]]
-fred.series1[fred.series1$Category == uis1[[8]], 'Category'] <- uis1[[7]]
-fred.series1[fred.series1$Category == uis1[[10]], 'Category'] <- uis1[[9]]
-fred.series1[fred.series1$Category == uis1[[12]], 'Category'] <- uis1[[11]]
-
-    # save a R object
-    save(fred.series1, file = "fred.series1.RData")
-
-
-## 2nd series unique 'Category' names
-uis2           <- unique(fred.series2$Category)
-
-# clean up 'CountyName' and 'Category'
-fred.series2[fred.series2$Category == uis2[[2]], 'CountyName'] <- right.counties
-fred.series2[fred.series2$Category == uis2[[4]], 'CountyName'] <- right.counties
-fred.series2[fred.series2$Category == uis2[[6]], 'CountyName'] <- right.counties    
-
-fred.series2[fred.series2$Category == uis2[[2]], 'Category'] <- uis2[[1]]
-fred.series2[fred.series2$Category == uis2[[4]], 'Category'] <- uis2[[3]]
-fred.series2[fred.series2$Category == uis2[[6]], 'Category'] <- uis2[[5]]
-
-    # save a R object
-    save(fred.series1, file = "fred.series1.RData")
+#     ## get the right county names
+#     right.counties <- c("Deaf Smith", "El Paso", "Fort Bend",
+#                         "Jeff Davis", "Jim Hogg", "Jim Wells",
+#                         "La Salle","Live Oak", "Palo Pinto",
+#                         "Red River", "San Augustine", "San Jacinto",
+#                         "San Patricio", "San Saba", "Tom Green",
+#                         "Val Verde", "Van Zandt")
+# 
+# ## 1st series unique 'Category' values
+# uis1           <- unique(fred.series1$Category)
+#     
+# # clean up 'CountyName' and 'Category'
+# fred.series1[fred.series1$Category == uis1[[3]], 'CountyName'] <- right.counties
+# fred.series1[fred.series1$Category == uis1[[4]], 'CountyName'] <- right.counties
+# fred.series1[fred.series1$Category == uis1[[6]], 'CountyName'] <- right.counties
+# fred.series1[fred.series1$Category == uis1[[8]], 'CountyName'] <- right.counties
+# fred.series1[fred.series1$Category == uis1[[10]], 'CountyName'] <- right.counties
+# fred.series1[fred.series1$Category == uis1[[12]], 'CountyName'] <- right.counties
+# fred.series1[fred.series1$Category == uis1[[14]], 'CountyName'] <- right.counties
+# 
+# fred.series1[fred.series1$Category == uis1[[3]], 'Category'] <- uis1[[1]]
+# fred.series1[fred.series1$Category == uis1[[4]], 'Category'] <- uis1[[2]]
+# fred.series1[fred.series1$Category == uis1[[6]], 'Category'] <- uis1[[5]]
+# fred.series1[fred.series1$Category == uis1[[8]], 'Category'] <- uis1[[7]]
+# fred.series1[fred.series1$Category == uis1[[10]], 'Category'] <- uis1[[9]]
+# fred.series1[fred.series1$Category == uis1[[12]], 'Category'] <- uis1[[11]]
+# 
+#     # save a R object
+#     save(fred.series1, file = "fred.series1.RData")
+# 
+# 
+# ## 2nd series unique 'Category' names
+# uis2           <- unique(fred.series2$Category)
+# 
+# # clean up 'CountyName' and 'Category'
+# fred.series2[fred.series2$Category == uis2[[2]], 'CountyName'] <- right.counties
+# fred.series2[fred.series2$Category == uis2[[4]], 'CountyName'] <- right.counties
+# fred.series2[fred.series2$Category == uis2[[6]], 'CountyName'] <- right.counties    
+# 
+# fred.series2[fred.series2$Category == uis2[[2]], 'Category'] <- uis2[[1]]
+# fred.series2[fred.series2$Category == uis2[[4]], 'Category'] <- uis2[[3]]
+# fred.series2[fred.series2$Category == uis2[[6]], 'Category'] <- uis2[[5]]
+# 
+#     # save a R object
+#     save(fred.series1, file = "fred.series1.RData")
 
 
 # Run the Data Function ---------------------------------------------------
 
 ## 1st series
-fred.obs1 <- lapply(seq_along(fred.series1$SeriesID), function(x){
-    
-    tryCatch({
-        
-        obs.scraper(myapi,fred.series1$SeriesID[x])
-    }, error = function(e) {
+    fred.obs1 <- lapply(seq_along(fred.series1$SeriesID), function(x){
         
         tryCatch({
-            Sys.sleep(3.5)
-            obs.scraper(myapi,fred.series1$SeriesID[x])
+            
+           obs.scraper(myapi,fred.series1$SeriesID[x])
         }, error = function(e) {
-            Sys.sleep(3.5)
-            obs.scraper(myapi,fred.series1$SeriesID[x])
+            
+            tryCatch({
+                Sys.sleep(3.5)
+                obs.scraper(myapi,fred.series1$SeriesID[x])
+            }, error = function(e) {
+                Sys.sleep(3.5)
+                obs.scraper(myapi,fred.series1$SeriesID[x])
+            })
         })
     })
-})
-    # give it names and save as R object
+# give it names and save as R object
     names(fred.obs1) <- fred.series1$SeriesID
     save(fred.obs1, file = "fred.obs1.RData")
 
 ## 2nd series
-fred.obs2 <- lapply(seq_along(fred.series2$SeriesID), function(x){
-    
-    tryCatch({
-        
-        obs.scraper(myapi,fred.series2$SeriesID[x])
-    }, error = function(e) {
+    fred.obs2 <- lapply(seq_along(fred.series2$SeriesID), function(x){
         
         tryCatch({
-            Sys.sleep(3.5)
-            obs.scraper(myapi,fred.series2$SeriesID[x])
+            
+           obs.scraper(myapi,fred.series2$SeriesID[x])
         }, error = function(e) {
-            Sys.sleep(3.5)
-            obs.scraper(myapi,fred.series2$SeriesID[x])
+            
+            tryCatch({
+                Sys.sleep(3.5)
+                obs.scraper(myapi,fred.series2$SeriesID[x])
+            }, error = function(e) {
+                Sys.sleep(3.5)
+                obs.scraper(myapi,fred.series2$SeriesID[x])
+            })
         })
     })
-})
-    # git is names and save as R object
+# git is names and save as R object
     names(fred.obs2) <- fred.series2$SeriesID
     save(fred.obs2, file = "fred.obs2.RData")
--- scraper.functions.R
+++ scraper.functions.R
@@ -9,32 +9,30 @@
 
 series.scraper <- function(key = NULL, id = NULL, filters = NULL){
     
-    if(is.null(key)){
-        stop("An API 'key' is required!")
-    }
-    if(is.null(id)){
-        stop("You must input a character vector corresponding to a release's 'id'")
-    }
+        # check if 'key' and 'id' inputs have been given
+            if(is.null(key)){
+                stop("An API 'key' is required!")
+            }
+            if(is.null(id)){
+                stop("You must input a character vector corresponding to a release's 'id'")
+            }
     
-    source("county.scraper.R")
-    source("category.scraper.R")
+    # set url variables, check 'id' data type, check for 'filter', and build the url
+        root <- "https://api.stlouisfed.org/fred/release/series?release_id="
+        usr  <- "&api_key="
+        if(!is.character(id)) {
+            id <- as.character(id)
+        }
+        if(!is.null(filters)){
+            filter <- paste("&tag_names=",
+                            paste(filters, collapse = ";"),
+                            sep = "")
+            url    <- paste(root, id, usr, key, filter, sep = "")
+        }   else {
+            url    <- paste(root, id, usr, key, sep = "")
+        }
     
-    root    <- "https://api.stlouisfed.org/fred/release/series?release_id="
-    cred    <- "&api_key="
-    
-    if(!is.character(id)) {
-        id <- as.character(id)
-    }
-    if(!is.null(filters)){
-        filter <- paste("&tag_names=",
-                        paste(filters, collapse = ";"),
-                        sep = ""
-        )
-        url    <- paste(root, id, cred, key, filter, sep = "")
-    }   else {
-        url    <- paste(root, id, cred, key, sep = "")
-    }
-    
+# run it
     html               <- url %>% read_html()
     series             <- html %>% html_nodes("series")
     series_id          <- series %>% html_attr("id")                        %>% as.character()
@@ -59,8 +57,9 @@ series.scraper <- function(key = NULL, id = NULL, filters = NULL){
                                      "LastUpdated" = last_updated,
                                      "Blessed"     = blessed_start
     )
-    data.info$CountyName <- county.scraper(title)
-    data.info$Category   <- category.scraper(title)
+    cat.county           <- cat.county.scraper(title)
+    data.info$CountyName <- sapply(seq_along(cat.county), function(y) cat.county[[y]][2] %>% tolower())
+    data.info$Category   <- sapply(seq_along(cat.county), function(y) cat.county[[y]][1])
     data.info
 }
 
@@ -75,59 +74,61 @@ series.scraper <- function(key = NULL, id = NULL, filters = NULL){
 
 obs.scraper <- function(key = NULL, series = NULL){
     
-    if(is.null(key)){
-        stop("An API 'key' is required!")
-    }
-    if(is.null(series)){
-        stop("You must input a character vector corresponding to a series' 'id'")
-    }
+    # check if 'key' and 'series' have been given
+        if(is.null(key)){
+            stop("An API 'key' is required!")
+        }
+        if(is.null(series)){
+            stop("You must input a character vector corresponding to a series' 'id'")
+        }
     
-    root        <- "https://api.stlouisfed.org/fred/series/observations?series_id="
-    cred        <- "&api_key="
-    url         <- paste(root, series, cred, key, sep = "")
+    # create url    
+        root <- "https://api.stlouisfed.org/fred/series/observations?series_id="
+        usr  <- "&api_key="
+        url  <- paste(root, series, usr, key, sep = "")
     
+# run it
     html        <- url %>% read_html() %>% html_nodes("observation")
     date        <- html %>% html_attr("date")  %>% as.Date()
     value       <- html %>% html_attr("value")
     value       <- ifelse(value == ".", NA, value) %>% as.numeric()
     data.values <- data.frame("Date" = date,
-                              "Value" = value
-    )
+                              "Value" = value)
 }
 
 
 # county.scraper(string) --------------------------------------------------
 
 
-county.scraper <- function(string){
-    
-    spaces.vector    <- gregexpr("\\s", string)
-    spaceless.vector <- sapply(seq_along(spaces.vector), function(x){
-        
-        target.space1 <- length(spaces.vector[[x]]) - 2
-        target.space2 <- length(spaces.vector[[x]]) - 1
-        target.pos1   <- spaces.vector[[x]][target.space1] + 1
-        target.pos2   <- spaces.vector[[x]][target.space2] - 1
-        substr(string[[x]], target.pos1, target.pos2)
-    })
-    spaceless.vector
-}
+# county.scraper <- function(string){
+#     
+#     spaces.vector    <- gregexpr("\\s", string)
+#     spaceless.vector <- sapply(seq_along(spaces.vector), function(x){
+#         
+#         target.space1 <- length(spaces.vector[[x]]) - 2
+#         target.space2 <- length(spaces.vector[[x]]) - 1
+#         target.pos1   <- spaces.vector[[x]][target.space1] + 1
+#         target.pos2   <- spaces.vector[[x]][target.space2] - 1
+#         substr(string[[x]], target.pos1, target.pos2)
+#     })
+#     spaceless.vector
+# }
 
 
 # category.scraper(string) ------------------------------------------------
 
 
-category.scraper <- function(string){
-    
-    spaces.vector    <- gregexpr("\\s", string)
-    spaceless.vector <- sapply(seq_along(spaces.vector), function(x){
-        
-        target.space1 <- length(spaces.vector[[x]]) - 3
-        target.pos1   <- spaces.vector[[x]][target.space1] - 1
-        substr(string[[x]], 1, target.pos1)
-    })
-    spaceless.vector
-}
+# category.scraper <- function(string){
+#     
+#     spaces.vector    <- gregexpr("\\s", string)
+#     spaceless.vector <- sapply(seq_along(spaces.vector), function(x){
+#         
+#         target.space1 <- length(spaces.vector[[x]]) - 3
+#         target.pos1   <- spaces.vector[[x]][target.space1] - 1
+#         substr(string[[x]], 1, target.pos1)
+#     })
+#     spaceless.vector
+# }
 
 
 # cat.county.scraper(title.vector) ----------------------------------------
@@ -136,48 +137,46 @@ category.scraper <- function(string){
 cat.county.scraper <- function(title.vector){
     
 # load dependencies
-require(dplyr, quietly = T)
-require(choroplethrMaps, quietly = T)
-data("county.regions")
+    require(dplyr, quietly = T)
+    require(choroplethrMaps, quietly = T)
+    data("county.regions")
+    extracted    <- list()
+    
+    # create vector of texas counties
+        tx.counties <- filter(county.regions, state.name == "texas") %>%
+                       select("CountyName" = county.name)
+        
 
-# create vector of texas counties
-tx.counties  <- filter(county.regions, state.name == "texas") %>%
-                select("CountyName" = county.name)
-# initialize the list to hold the lapply() results 
-extracted    <- list()
-
-# begin function
-        cat.county <-lapply(seq_along(title.vector), function(x){
+###--- BEGIN lapply() FUN
+cat.county <-lapply(seq_along(title.vector), function(x){
             
-                # find county position
-                county.found <- paste(tx.counties[[1]], collapse = "|") %>%
-                    gregexpr(title.vector[x], ignore.case = TRUE)
-                # extract from start to 2 spaces before the start of the county
-                first.extract <- substr(x     = title.vector[x],
-                                        start = 1,
-                                        stop  = county.found[[1]][1] - 2)
-                
-                # extract the 'Category'
-                        # find position of either 'in' or 'for' at the end of the string
-                        end.found     <- gregexpr("in$|for$", first.extract)
-                        # extract from start to 2 spaces before either 'in' or 'for'
-                        Category      <- substr(x     = first.extract,
-                                                start = 1,
-                                                stop  = end.found[[1]][1] - 2)
-                
-                # extract the 'County'
-                        # find the space before 'County'
-                        county.found2 <- gregexpr(" County,", title.vector[x])
-                        # extract from start of county name (county.found) to one space before the comma
-                        CountyName    <- substr(x     = title.vector[x],
-                                                start = county.found[[1]][1],
-                                                stop  = county.found2[[1]][1] - 1)
-                
-                extracted[[x]] <- c(Category, CountyName)
-
-            
+        # find county position
+            county.found <- paste(tx.counties[[1]], collapse = "|") %>%
+                            gregexpr(title.vector[x], ignore.case = TRUE)
+        
+                # start extracting 'Category'
+                    first.extract <- substr(x     = title.vector[x],
+                                            start = 1,
+                                            stop  = county.found[[1]][1] - 2)
+                    end.found     <- gregexpr("in$|for$", first.extract)
+                    Category      <- substr(x     = first.extract,
+                                            start = 1,
+                                            stop  = end.found[[1]][1] - 2)
+                    
+                # start exracting 'County'
+                    county.found2 <- gregexpr(" County,", title.vector[x])
+                    CountyName    <- substr(x     = title.vector[x],
+                                            start = county.found[[1]][1],
+                                            stop  = county.found2[[1]][1] - 1)
+        
+        # load data into list
+            extracted[[x]] <- c(Category, CountyName)
+        
             })
-cat.county
+###--- END lapply() FUN
+
+# return the list
+    cat.county
 }
 
 
